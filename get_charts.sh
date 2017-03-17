a20aeb89527824f1745e8dbc2b892ef866e5fee4
Steven Ryland <stevenry@microsoft.com>
2017-03-16 20:05:37 -0700

    adding Users to get_charts.sh, fixing values.yaml with new ref update

--- get_charts.sh
+++ get_charts.sh
@@ -3,3 +3,4 @@ mkdir -p contoso-bikerental/charts
 cp -R Bikes/contoso-bikerental-bikes/. contoso-bikerental/charts/contoso-bikerental-bikes/
 cp -R Gateway/contoso-bikerental-gateway/. contoso-bikerental/charts/contoso-bikerental-gateway/
 cp -R ReservationEngine/contoso-bikerental-reservationengine/. contoso-bikerental/charts/contoso-bikerental-reservationengine/
+cp -R Users/contoso-bikerental-users/. contoso-bikerental/charts/contoso-bikerental-users/
