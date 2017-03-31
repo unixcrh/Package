8508de27a05abe668133404e37d1e187f44aecea
Steven Ryland <stevenry@microsoft.com>
2017-03-31 15:38:09 -0700

    end of sprint demo!

--- build_tag_push.sh
+++ build_tag_push.sh
@@ -12,9 +12,15 @@ docker build -t "$1/contoso-bikerental-gateway:$2" Gateway/app/
 docker build -t "$1/contoso-bikerental-reservationengine:$2" ReservationEngine/app/
 docker build -t "$1/contoso-bikerental-users:$2" Users/app/
 docker build -t "$1/contoso-bikerental-devsite:$2" DevSite/
+docker build -t "$1/contoso-bikerental-billing:$2" Billing/
+docker build -t "$1/contoso-bikerental-reservations:$2" Reservation/app/
+docker build -t "$1/contoso-bikerental-notification:$2" Notification/
 
 docker push "$1/contoso-bikerental-bikes:$2"
 docker push "$1/contoso-bikerental-gateway:$2"
 docker push "$1/contoso-bikerental-reservationengine:$2"
 docker push "$1/contoso-bikerental-users:$2"
 docker push "$1/contoso-bikerental-devsite:$2"
+docker push "$1/contoso-bikerental-billing:$2"
+docker push "$1/contoso-bikerental-reservations:$2"
+docker push "$1/contoso-bikerental-notification:$2"
--- get_charts.sh
+++ get_charts.sh
@@ -5,3 +5,6 @@ cp -R Gateway/contoso-bikerental-gateway/. contoso-bikerental/charts/contoso-bik
 cp -R ReservationEngine/contoso-bikerental-reservationengine/. contoso-bikerental/charts/contoso-bikerental-reservationengine/
 cp -R Users/contoso-bikerental-users/. contoso-bikerental/charts/contoso-bikerental-users/
 cp -R DevSite/contoso-bikerental-devsite/. contoso-bikerental/charts/contoso-bikerental-devsite/
+cp -R Billing/contoso-bikerental-billing/. contoso-bikerental/charts/contoso-bikerental-billing/
+cp -R Reservation/contoso-bikerental-reservations/. contoso-bikerental/charts/contoso-bikerental-reservations/
+cp -R Notification/contoso-bikerental-notification/. contoso-bikerental/charts/contoso-bikerental-notification/
