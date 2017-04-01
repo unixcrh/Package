a57bd3869411fa7ce98e5eca979bcf719aeb55e3
Steven Ryland <stevenry@microsoft.com>
2017-04-01 11:31:19 -0700

    update build_tag_push.sh with new location of dockerfiles

--- build_tag_push.sh
+++ build_tag_push.sh
@@ -8,12 +8,12 @@ if [ -z "$2" ]; then
 fi
 
 docker build -t "$1/contoso-bikerental-bikes:$2" Bikes/
-docker build -t "$1/contoso-bikerental-gateway:$2" Gateway/app/
-docker build -t "$1/contoso-bikerental-reservationengine:$2" ReservationEngine/app/
-docker build -t "$1/contoso-bikerental-users:$2" Users/app/
+docker build -t "$1/contoso-bikerental-gateway:$2" Gateway/
+docker build -t "$1/contoso-bikerental-reservationengine:$2" ReservationEngine/
+docker build -t "$1/contoso-bikerental-users:$2" Users/
 docker build -t "$1/contoso-bikerental-devsite:$2" DevSite/
 docker build -t "$1/contoso-bikerental-billing:$2" Billing/
-docker build -t "$1/contoso-bikerental-reservations:$2" Reservation/app/
+docker build -t "$1/contoso-bikerental-reservations:$2" Reservation/
 docker build -t "$1/contoso-bikerental-notification:$2" Notification/
 
 docker push "$1/contoso-bikerental-bikes:$2"
